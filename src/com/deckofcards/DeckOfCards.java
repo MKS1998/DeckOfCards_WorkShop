69c2906b66a45f08cd3dc78ac5d976fabe3e4e8d
MKS1998 <nksayyad2900@outlook.com>
2021-10-28 20:33:00 +0530

    Added : Initializing the deck of cards

---
+++ src/com/deckofcards/DeckOfCards.java
@@ -0,0 +1,9 @@
+package com.deckofcards;
+
+public class DeckOfCards {
+	public static void main(String[] args) {
+		System.out.println("Welcome into the Decks of Cards!!! Thank you");
+		CardsPlay cardsPlay = new CardsPlay();
+		cardsPlay.generateUniqueCards();
+	}
+}
