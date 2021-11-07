050bdf6c23bafba99d208325fa0e5ae893fc14b6
MKS1998 <nksayyad2900@outlook.com>
2021-11-07 12:05:29 +0530

    Added : Program for the card recieve and distibute to the player

--- src/com/deckofcards/DeckOfCards.java
+++ src/com/deckofcards/DeckOfCards.java
@@ -4,7 +4,9 @@ public class DeckOfCards {
 	public static void main(String[] args) {
 		System.out.println("Welcome into the Decks of Cards");
 		CardsPlay cardsPlay = new CardsPlay();
-		cardsPlay.generateUniqueCards();
+		cardsPlay.generateCards();
 		cardsPlay.addPlayer();
+		cardsPlay.distributeCards();
+        cardsPlay.printCardList();
 	}
 }
--- src/com/deckofcards/Player.java
+++ src/com/deckofcards/Player.java
@@ -1,32 +1,25 @@
 package com.deckofcards;
 
 
+
+import java.util.ArrayList;
+
 public class Player {
-    public static void deckOfcards(){
-        String[] suits = {"Clubs", "Daimonds", "Hearts","Spades"};
-        String[] ranks = {"2", "3", "4","5","6","7","8","9","10","Jack","King","Queen","Ace"};
+    String name;
+    ArrayList<String> cardList = new ArrayList<>();
 
-        int sizeOfDeck = suits.length * ranks.length;
-        System.out.println("Number of cards in the Deck:"+sizeOfDeck);
-        String[] deck = new String[sizeOfDeck];
-        for( int i = 0; i< ranks.length; i++){
-            for(int j=0; j<suits.length; j++){
-                deck[suits.length*i+j]=ranks[i] +"->" +suits[j];
-            }
-        }
-        toPrint(deck);
+    public Player(String name) {
+        this.name = name;
     }
 
-    private static void toPrint(String[] deck) {
-        System.out.println("Cards in Deck:");
-        for(String element : deck){
-            System.out.print("\t" + element);
+    public void setCardList(String card) {
+        cardList.add(card);
+    }
+
+    public void getCardList() {
+        //get all cards inside cardList
+        for (String position : cardList) {
+            System.out.print(position + ", ");
         }
     }
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to Deck of Cards Program");
-        Player.deckOfcards();
-    }
-
 }
