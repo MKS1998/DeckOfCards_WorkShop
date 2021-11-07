73b4f137f70fa64bea380cef47d77a7eb7ab2f79
MKS1998 <nksayyad2900@outlook.com>
2021-11-07 11:57:40 +0530

    Added : program for the Add Player into the game

--- src/com/deckofcards/CardsPlay.java
+++ src/com/deckofcards/CardsPlay.java
@@ -1,5 +1,7 @@
 package com.deckofcards;
 
+import java.util.Scanner;
+
 public class CardsPlay {
 	String[] cards = new String[52];
 
@@ -10,11 +12,27 @@ public class CardsPlay {
 		int position = 0;
 		for (int i = 0; i < suit.length; i++) {
 			for (int j = 0; j < rank.length; j++) {
-
 				cards[position] = suit[i] + rank[j];
 				position++;
 				System.out.println(cards[position] = suit[i] + rank[j]);
 			}
 		}
+	}
+
+	public void addPlayer(){
+	        System.out.print(" Enter the No of players : ");
+	        Scanner sc = new Scanner(System.in);
+	        int playerCount = sc.nextInt();
+	        if(playerCount >= 2 && playerCount <= 4){
+	            for(int i=0; i<playerCount; i++){
+	                Player player = new Player();
+	                player.add(player);
+	            }
+	        }
+	        else
+	        {
+	            System.out.println("Invalid input Enter between 2 to 4 ");
+	        }
+
 	}
 }
--- src/com/deckofcards/DeckOfCards.java
+++ src/com/deckofcards/DeckOfCards.java
@@ -2,8 +2,9 @@ package com.deckofcards;
 
 public class DeckOfCards {
 	public static void main(String[] args) {
-		System.out.println("Welcome into the Decks of Cards!!! Thank you");
+		System.out.println("Welcome into the Decks of Cards");
 		CardsPlay cardsPlay = new CardsPlay();
 		cardsPlay.generateUniqueCards();
+		cardsPlay.addPlayer();
 	}
 }
